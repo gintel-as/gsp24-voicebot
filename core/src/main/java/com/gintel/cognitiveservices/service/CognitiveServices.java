ee3f18316c85bc96eaaed3f269d90db6cbfc71e1
OstengenHerman <herman.ostengen@gintel.com>
2024-06-25 08:37:59 +0200

    fix: filter "RECOGNIZED" messages with no content from being sent to AI

--- core/src/main/java/com/gintel/cognitiveservices/service/CognitiveServices.java
+++ core/src/main/java/com/gintel/cognitiveservices/service/CognitiveServices.java
@@ -59,9 +59,10 @@ public class CognitiveServices implements CommunicationServiceListener {
             try {
 //                session.getBasicRemote().sendText(e.toString());
                 if (e instanceof SpeechToTextEvent) {
-                    if (e.toString().charAt(9) == 'D') {
+                    service.playMedia(event.getSessionId(), e.toString());
+                    if (e.toString().charAt(9) == 'D' && e.toString().length() > 12) {
                         for (Openai ai : getServices(Openai.class)){
-                            service.playMedia(event.getSessionId(), e.toString());
+                            
                             service.playMedia(event.getSessionId(), "stop_recording");
                             long t1 = System.currentTimeMillis();
                             OpenaiResult aiResult = ai.openai(e.toString().replaceAll("RECOGNIZED: ", ""), ctx, null, null);
