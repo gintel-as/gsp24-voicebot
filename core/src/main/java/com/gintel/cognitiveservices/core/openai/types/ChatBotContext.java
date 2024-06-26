38770614c79e263d139e41889a06dbffa18005f8
OstengenHerman <herman.ostengen@gintel.com>
2024-06-26 16:18:05 +0200

    feat: implemented chat-deletetion based on "primitive" token management
    NB! Token usage is based on exactly one token per 4 characters

--- core/src/main/java/com/gintel/cognitiveservices/core/openai/types/ChatBotContext.java
+++ core/src/main/java/com/gintel/cognitiveservices/core/openai/types/ChatBotContext.java
@@ -1,10 +1,13 @@
 package com.gintel.cognitiveservices.core.openai.types;
 
 import com.azure.ai.openai.models.ChatRequestMessage;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChatBotContext {
     private List<ChatRequestMessage> conversation;
+    private List<Integer> messageTokens = new ArrayList<>();
 
     public void addMessages(List<ChatRequestMessage> chatMessages) {
         if (chatMessages != null) {this.conversation = chatMessages;}
@@ -12,5 +15,17 @@ public class ChatBotContext {
 
     public List<ChatRequestMessage> getMessages(){
         return this.conversation;
+    }
+
+    public void addTokenCost(Integer tokens) {
+        this.messageTokens.add(tokens);
+    }
+
+    public void removeTokenCost(Integer index) {
+        this.messageTokens.remove(index);
+    }
+
+    public List<Integer> getMessageTokens(){
+        return this.messageTokens;
     }
 }
--- openai-azure/src/main/java/com/gintel/cognitiveservices/openai/azure/AzureOpenaiService.java
+++ openai-azure/src/main/java/com/gintel/cognitiveservices/openai/azure/AzureOpenaiService.java
@@ -18,6 +18,7 @@ import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.ai.openai.models.ChatChoice;
 import com.azure.ai.openai.models.ChatCompletions;
 import com.azure.ai.openai.models.ChatCompletionsOptions;
+import com.azure.ai.openai.models.ChatRequestAssistantMessage;
 import com.azure.ai.openai.models.ChatRequestMessage;
 import com.azure.ai.openai.models.ChatRequestSystemMessage;
 import com.azure.ai.openai.models.ChatRequestUserMessage;
@@ -51,15 +52,22 @@ public class AzureOpenaiService implements Openai{
 
 
             List<ChatRequestMessage> chatMessages = new ArrayList<>();
-            chatMessages.add(new ChatRequestSystemMessage("You are a helpful assistant."));
-            if (ctx.getMessages() != null){chatMessages = ctx.getMessages();}
+            if (ctx.getMessages() != null){
+                chatMessages = ctx.getMessages();
+            } else {
+                chatMessages.add(new ChatRequestSystemMessage("You are a helpful assistant. Reply in a conversational language with only the necessary content."));
+                ctx.addTokenCost("You are a helpful assistant.".length()*4);
+            }
 
             chatMessages.add(new ChatRequestUserMessage(text));
-            if (chatMessages != null) {ctx.addMessages(chatMessages);}
+            if (chatMessages != null) {
+                ctx.addMessages(chatMessages);
+                ctx.addTokenCost(text.length()*4);
+            }
 
 
             ChatCompletionsOptions completionsOptions = new ChatCompletionsOptions(chatMessages);
-            completionsOptions.setMaxTokens(500);
+            completionsOptions.setMaxTokens(1000);
             
             ChatCompletions chatCompletions = client.getChatCompletions(deploymentOrModelId, completionsOptions);
             CompletionsUsage usage = chatCompletions.getUsage();
@@ -69,8 +77,49 @@ public class AzureOpenaiService implements Openai{
                 logger.info("Total tokens used: {}", usage.getTotalTokens());
 
                 if (usage.getTotalTokens() >= completionsOptions.getMaxTokens()) {
-                    logger.warn("The maximum number of tokens has been reached.");
-                    return new OpenaiResult(OpenaiStatus.ERROR, "Maximum token limit reached", text);
+                    Integer messages = 1;
+                    Integer diff = usage.getTotalTokens() - completionsOptions.getMaxTokens();
+                    while(messages < ctx.getMessageTokens().size()-1){
+                        Integer sum = 0;
+                        for (int i = 0; i < messages; i++) {
+                            sum += ctx.getMessageTokens().get(i+1);
+                        }
+                        if (sum > diff) {
+                            for (int j = messages; j > 0; j--){
+                                ctx.removeTokenCost(j);
+                                chatMessages.remove(j);
+                                ctx.addMessages(chatMessages);
+                            }
+                            completionsOptions = new ChatCompletionsOptions(chatMessages);
+                            chatCompletions = client.getChatCompletions(deploymentOrModelId, completionsOptions);
+                            usage = chatCompletions.getUsage();
+                            if (usage != null) {
+                                logger.info("Deleted " + messages + " messages");
+                                logger.info("Prompt tokens used (after deletion): {}", usage.getPromptTokens());
+                                logger.info("Completion tokens used (after deletion): {}", usage.getCompletionTokens());
+                                logger.info("Total tokens used (after deletion): {}", usage.getTotalTokens());
+                                logger.info("" + chatMessages);
+                            }
+                            String mld = "";
+                            for (ChatChoice choice : chatCompletions.getChoices()) {
+                                ChatResponseMessage message = choice.getMessage();
+                                System.out.printf("Index: %d, Chat Role: %s.%n", choice.getIndex(), message.getRole());
+                                System.out.println("Message:");
+                                System.out.println(message.getContent());
+                                if ("assistant".equals(message.getRole().toString())) {
+                                    mld = message.getContent();
+                                    chatMessages.add(new ChatRequestAssistantMessage(message.getContent()));
+                                    ctx.addMessages(chatMessages);
+                                    ctx.addTokenCost(message.getContent().length()*4);
+                                }
+                            }
+
+                            return new OpenaiResult(OpenaiStatus.OK, "NB! Token management deleted older messages\n\n" + mld, text);
+                        }
+                        messages += 2;
+                    };
+                    logger.warn("Maximum token limit reached with previous prompt.");
+                    return new OpenaiResult(OpenaiStatus.ERROR, "Maximum token limit reached with previous prompt", text);
                 }
             }
 
@@ -82,6 +131,9 @@ public class AzureOpenaiService implements Openai{
                 System.out.println(message.getContent());
                 if ("assistant".equals(message.getRole().toString())) {
                     mld = message.getContent();
+                    chatMessages.add(new ChatRequestAssistantMessage(message.getContent()));
+                    ctx.addMessages(chatMessages);
+                    ctx.addTokenCost(message.getContent().length()*4);
                 }
             }
 
