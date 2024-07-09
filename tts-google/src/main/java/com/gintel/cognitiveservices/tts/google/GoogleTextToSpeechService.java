27d1634001fab7cd315bd6d9e7adbeb7aca620af
panteajoo <panjoo2000@gmail.com>
2024-07-09 13:56:37 +0200

    google tts started implementation

--- tts-google/src/main/java/com/gintel/cognitiveservices/tts/google/GoogleTextToSpeechService.java
+++ tts-google/src/main/java/com/gintel/cognitiveservices/tts/google/GoogleTextToSpeechService.java
@@ -1,51 +1,119 @@
-package com.gintel.cognitiveservices.tts.google;
-
-// Imports the Google Cloud client library
-import com.google.cloud.texttospeech.v1.AudioConfig;
-import com.google.cloud.texttospeech.v1.AudioEncoding;
-import com.google.cloud.texttospeech.v1.SsmlVoiceGender;
-import com.google.cloud.texttospeech.v1.SynthesisInput;
-import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
-import com.google.cloud.texttospeech.v1.TextToSpeechClient;
-import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
+import com.google.api.gax.rpc.ApiStreamObserver;
+import com.google.api.gax.rpc.BidiStreamingCallable;
+import com.google.cloud.texttospeech.v1.*;
 import com.google.protobuf.ByteString;
-import java.io.FileOutputStream;
+import com.google.protobuf.FieldMask;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.api.gax.core.FixedCredentialsProvider;
 
+import java.io.FileOutputStream;
 public class GoogleTextToSpeechService {
-    /** Demonstrates using the Text-to-Speech API. */
-public static void main(String... args) throws Exception {
-    // Instantiates a client
-    try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
-      // Set the text input to be synthesized
-      SynthesisInput input = SynthesisInput.newBuilder().setText("Hello, World!").build();
-  
-      // Build the voice request, select the language code ("en-US") and the ssml voice gender
-      // ("neutral")
-      VoiceSelectionParams voice =
-        VoiceSelectionParams.newBuilder()
-          .setLanguageCode("en-US")
-          .setSsmlGender(SsmlVoiceGender.NEUTRAL)
-          .build();
-  
-      // Select the type of audio file you want returned
-      AudioConfig audioConfig =
-        AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.MP3).build();
-  
-      // Perform the text-to-speech request on the text input with the selected voice parameters and
-      // audio file type
-      SynthesizeSpeechResponse response =
-        textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
-  
-      // Get the audio contents from the response
-      ByteString audioContents = response.getAudioContent();
-  
-      // Write the response to the output file.
-      try (OutputStream out = new FileOutputStream("output.mp3")) {
-        out.write(audioContents.toByteArray());
-        System.out.println("Audio content written to file \"output.mp3\"");
-      }
+
+    private TextToSpeechSettings settings;
+    private TextToSpeechClient client;
+
+    public GoogleTextToSpeechService(String credentialsPath) throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath))
+        .createScoped("https://www.googleapis.com/auth/cloud-platform");
+
+        settings = TextToSpeechSettings.newBuilder()
+                .setCredentialsProvider(() -> GoogleCredentials.fromStream(new FileInputStream(credentialsPath)))
+                .build();
+        client = TextToSpeechClient.create(settings);
     }
-  }
-    
+
+    public void textToSpeech(String text, String languageCode, String voiceName, String outputFormat, String outputFile) {
+        SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+
+        VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+                .setLanguageCode(languageCode)
+                .setName(voiceName)
+                .build();
+
+        AudioConfig audioConfig = AudioConfig.newBuilder()
+                .setAudioEncoding(AudioEncoding.valueOf(outputFormat))
+                .build();
+
+        SynthesizeSpeechResponse response = client.synthesizeSpeech(input, voice, audioConfig);
+
+        ByteString audioContents = response.getAudioContent();
+
+        try (OutputStream out = new FileOutputStream(outputFile)) {
+            out.write(audioContents.toByteArray());
+            System.out.println("Audio content written to file " + outputFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void textToStream(String text, String languageCode, String voiceName, String outputFormat, OutputStream outputStream) {
+        SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+
+        VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+                .setLanguageCode(languageCode)
+                .setName(voiceName)
+                .build();
+
+        AudioConfig audioConfig = AudioConfig.newBuilder()
+                .setAudioEncoding(AudioEncoding.valueOf(outputFormat))
+                .build();
+
+        SynthesizeSpeechResponse response = client.synthesizeSpeech(input, voice, audioConfig);
+
+        ByteString audioContents = response.getAudioContent();
+
+        try {
+            outputStream.write(audioContents.toByteArray());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startTextToSpeechSession(String sessionId, String text, String languageCode, String voiceName, String outputFormat, EventHandler eventHandler) {
+        SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+
+        VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+                .setLanguageCode(languageCode)
+                .setName(voiceName)
+                .build();
+
+        AudioConfig audioConfig = AudioConfig.newBuilder()
+                .setAudioEncoding(AudioEncoding.valueOf(outputFormat))
+                .build();
+
+        BidiStreamingCallable<SynthesizeSpeechRequest, SynthesizeSpeechResponse> callable = client.synthesizeSpeechCallable();
+
+        ApiStreamObserver<SynthesizeSpeechResponse> responseObserver = new ApiStreamObserver<SynthesizeSpeechResponse>() {
+            @Override
+            public void onNext(SynthesizeSpeechResponse response) {
+                ByteString audioContents = response.getAudioContent();
+                eventHandler.onAudioChunk(audioContents.toByteArray());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                eventHandler.onError(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                eventHandler.onComplete();
+            }
+        };
+
+        ApiStreamObserver<SynthesizeSpeechRequest> requestObserver = callable.bidiStreamingCall(responseObserver);
+        requestObserver.onNext(SynthesizeSpeechRequest.newBuilder().setInput(input).setVoice(voice).setAudioConfig(audioConfig).build());
+        requestObserver.onCompleted();
+    }
+
+    public interface EventHandler {
+        void onAudioChunk(byte[] audioChunk);
+
+        void onError(Throwable t);
+
+        void onComplete();
+    }
 }
